b8813bf5bce121878defabd14005f9021f8bad19
Paul <web3samuraidev@gmail.com>
2024-01-26 21:34:44 -0400

    chore: update everything to oort

--- environments/next/src/app/client.tsx
+++ environments/next/src/app/client.tsx
@@ -2,26 +2,26 @@
 
 import { useEffect, useState } from 'react'
 import { http, createPublicClient, webSocket } from 'viem'
-import { mainnet } from 'viem/chains'
+import { oortMainnetDev } from 'viem/chains'
 
 export function Client() {
   const [success, setSuccess] = useState<boolean | undefined>()
   useEffect(() => {
     ;(async () => {
       const client = createPublicClient({
-        chain: mainnet,
+        chain: oortMainnetDev,
         transport: http(),
       })
 
-      const webSocketClient = createPublicClient({
-        chain: mainnet,
+      /*const webSocketClient = createPublicClient({
+        chain: oortMainnetDev,
         transport: webSocket(
           'wss://eth-mainnet.g.alchemy.com/v2/4iIl6mDHqX3GFrpzmfj2Soirf3MPoAcH',
         ),
-      })
+      })*/
 
       await client.getBlockNumber()
-      await webSocketClient.getBlockNumber()
+      //await webSocketClient.getBlockNumber()
 
       setSuccess(true)
     })()
--- environments/next/src/app/page.tsx
+++ environments/next/src/app/page.tsx
@@ -1,22 +1,14 @@
 import { http, createPublicClient, webSocket } from 'viem'
-import { mainnet } from 'viem/chains'
+import { oortMainnetDev } from 'viem/chains'
 import { Client } from './client'
 
 export default async function Home() {
   const client = createPublicClient({
-    chain: mainnet,
+    chain: oortMainnetDev,
     transport: http(),
   })
 
-  const webSocketClient = createPublicClient({
-    chain: mainnet,
-    transport: webSocket(
-      'wss://eth-mainnet.g.alchemy.com/v2/4iIl6mDHqX3GFrpzmfj2Soirf3MPoAcH',
-    ),
-  })
-
   await client.getBlockNumber()
-  await webSocketClient.getBlockNumber()
 
   return (
     <>
